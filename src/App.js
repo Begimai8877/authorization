24f4cafbc6c9cd046db276fa3d7e442509b60011
Begimai <begimai.takeeva.ch@gmail.com>
2023-02-14 13:47:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,270 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const [isShown, setIsShown] = useState(false);
+
+  // const aaaa = async () => {
+  //   const data = await fetch('http://semanyan.pythonanywhere.com/api/v1/achievements/list/?format=api', {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     }
+  //   }).then(res => res.json())
+  //   .then(res => {
+  //       console.log(res);
+  //   })
+
+  //   console.log(data, "Aaaaa");
+
+  // }
+
+  // useEffect(() => {
+  //   aaaa()
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav
+        id="header"
+        class="w-full z-30 top-10 py-1 bg-white shadow-lg border-b border-blue-400"
+      >
+        <div class="w-full flex items-center justify-between mt-0 px-6 py-2">
+          <label for="menu-toggle" class="cursor-pointer md:hidden block">
+            <svg
+              class="fill-current text-blue-600"
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+            >
+              <title>menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+            </svg>
+          </label>
+          <input class="hidden" type="checkbox" id="menu-toggle" />
+
+          <div
+            class="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1"
+            id="menu"
+          >
+            <nav>
+              <ul class="md:flex items-center justify-between text-base text-blue-600 pt-4 md:pt-0">
+                <li>
+                  <a
+                    class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2"
+                    href="#"
+                  >
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="inline-block no-underline hover:text-black font-medium text-lg py-2 px-4 lg:-ml-2"
+                    href="#"
+                  >
+                    About
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div
+            class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4"
+            id="nav-content"
+          >
+            <div
+              class="order-2 md:order-3 flex flex-wrap items-center justify-end mr-0 md:mr-4"
+              id="nav-content"
+            >
+              <div class="auth flex items-center w-full md:w-full">
+                <button class="bg-transparent text-gray-800  p-2 rounded border border-gray-300 mr-4 hover:bg-gray-100 hover:text-gray-700">
+                  Sign in
+                </button>
+                <button
+                  class="bg-blue-600 text-gray-200  p-2 rounded  hover:bg-blue-500 hover:text-gray-100"
+                  onClick={() => setIsShown(true)}
+                >
+                  Sign up
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <link
+        rel="stylesheet"
+        href="https://cdn.tailgrids.com/tailgrids-fallback.css"
+      />
+
+      <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+        <div class="container">
+          <div class="flex flex-wrap -mx-4">
+            <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+              <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                  alt="image"
+                  class="w-full"
+                />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                    >
+                      50+ Best creative website themes & templates
+                    </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                    Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                    elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                    href="javascript:void(0)"
+                    class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                  >
+                    View Details
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+              <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
+                  alt="image"
+                  class="w-full"
+                />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                    >
+                      The ultimate UX and UI guide to card design
+                    </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                    Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                    elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                    href="javascript:void(0)"
+                    class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                  >
+                    View Details
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+              <div class="bg-white rounded-lg overflow-hidden mb-10">
+                <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-03.jpg"
+                  alt="image"
+                  class="w-full"
+                />
+                <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                    <a
+                      href="javascript:void(0)"
+                      class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                    >
+                      Creative Card Component designs graphic elements
+                    </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                    Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                    elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                    href="javascript:void(0)"
+                    class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                  >
+                    View Details
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {isShown && <Modal setIsShown={setIsShown} />}
     </div>
   );
 }
